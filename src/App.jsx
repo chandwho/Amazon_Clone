6745dac7b7fb73249a3a6ba087557fc7be1441d8
Chandan <ch7.pandey@gmail.com>
2023-04-24 18:41:13 +0530

    Add routes and listener for user login/logout
    Add routes for Checkout add Orders page.
    Add onAuthStateChanged to listen to the user login/logout to keep track
    of the users.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,45 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import Checkout from './pages/Checkout'
-import Navbar from './pages/components/Navbar'
+import SignUp from './pages/SignUp'
+import { authActions } from './store/authSlice';
+import { useDispatch, useSelector } from 'react-redux'
+import {onAuthStateChanged} from 'firebase/auth';
+import { auth } from './firebaseConfig';
+import Orders from './pages/Orders'
 
 export default function App() {
+
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.auth)
+
+  // Listens to user login and logout and dispatches used details(renders only once when App loads)
+  React.useEffect(() =>{
+    onAuthStateChanged(auth, (currentUser) =>{
+      if(currentUser){
+        console.log(currentUser)
+        // dispatch(authActions.setUser(currentUser))
+        dispatch(authActions.signIn({
+          email: currentUser.email,
+          name: currentUser.displayName
+        }))
+      }else{
+        dispatch(authActions.logOut())
+      }
+    })
+  },[])
+
+
+
   return (
     <div>
-      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}>
         </Route>
-        <Route path='/Signin' element={<SignIn/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
         <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='/orders' element={<Orders/>}/>
       </Routes>
     </div>
   )

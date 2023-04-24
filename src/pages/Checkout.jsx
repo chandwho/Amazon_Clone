8685bd8ce4aec9771f4f8cc9d41ab04984929cf1
Chandan <ch7.pandey@gmail.com>
2023-04-24 18:50:02 +0530

    Add firebase firestore to save product ProductData
    Add onClick listener to place order button
    Save order to firestore using updateDoc
    Reset redux cartItems state to empty array after orders are placed

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 import CheckoutItems from '../components/CheckoutItems'
+import Navbar from '../components/Navbar'
+import { db } from '../firebaseConfig'
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore'
+import { cartActions } from '../store/cartSlice'
 
 export default function Checkout() {
+
   const cartItems = useSelector(state => state.cart.cartItems)
   const cartCount = useSelector(state => state.cart.totalQuantity)
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const user = useSelector(state => state.auth.user)
 
   //Total price of cart
   const totalPrice = cartItems.reduce((total,item) =>{
@@ -19,7 +29,7 @@ export default function Checkout() {
         id = {item.id}
         url = {item.url}
         brandName = {item.brandName}
-        productName = {item.productName}
+        productName =  {item.productName}
         rating = {item.rating}
         price = {item.price}
         quantity = {item.quantity}
@@ -27,35 +37,75 @@ export default function Checkout() {
       />
     )
   })
+
+  const saveOrders = () =>{
+    //If user is logged in
+    if(user){
+      cartItems.map((item) =>{
+        const itemRef = doc(db, 'users', `${user.email}`)
+        updateDoc(itemRef,
+          {cart: arrayUnion({
+            id: item.id,
+            url: item.url,
+            brandName: item.brandName,
+            productName: item.productName,
+            rating: item.rating,
+            price: item.price,
+            quantity: item.quantity,
+            collectivePrice: item.collectivePrice,
+            })
+          }
+        ).then(dispatch(cartActions.resetCart()))  
+      })
+      alert('Hurray! Your order has been placed.')  
+    }else{
+      navigate('/signin')
+    }
+    }
+  
   return (
-    <div className='mt-[55px] bg-gray-200 p-5 text-sm md:text-lg'>
-      {
-      cartItems.length===0?
-      // When cart is empty
-        <div className='h-[150px] w-full bg-white p-4 flex flex-col justify-center gap-3'>
-          <h1 className='text-2xl font-semibold'>Your Amazon Cart is empty.</h1>
-          <Link to='/'>
-          <p className='cursor-pointer text-[15px] hover:text-blue-700'>Continue Shopping</p>
-          </Link>  
-        </div>
-        :
-        // When cart has items
-        <div className='mt-[100px] md:mt-0'>
-          <h1 className='text-xl md:text-3xl font-semibold mb-7'>Shopping Cart</h1>
-          <div className='flex flex-col md:flex-row gap-5'>
-            {/* Cart Items*/}
-            <div className='flex flex-col gap-3 md:w-[75%]'>
-              {checkoutItems}
-            </div>
-            {/*Section to display total price of cart*/}
-            <div className='p-5 flex flex-col justify-center items-center gap-3 md:w-[23%] max-h-[220px] border bg-white rounded shadow-xl'>
-              <p className='text-[17px] md:text-[20px]'>Subtotal of ({cartCount} items): <strong>₹{totalPrice}</strong></p>
-              <button className='bg-[#FFD814] hover:bg-yellow-500 border-[#c9a801] border h-9 rounded px-2 w-full'
-                  >Proceed to buy</button>
+    <div>
+      <Navbar/>
+      <div className='mt-[110px] md:mt-[55px] bg-gray-200 p-5 text-sm md:text-lg'>
+        {/* <h2>Hello, {user? user?.email:''}</h2> */}
+        {
+        cartItems.length===0?
+        // When cart is empty
+          <div className='h-[150px] w-full bg-white p-4 flex flex-col justify-center gap-3'>
+            <h1 className='text-2xl font-semibold'>Your Amazon Cart is empty.</h1>
+            {user?  
+              <Link to='/'>
+                <p className='cursor-pointer text-[15px] hover:text-blue-700'>Continue Shopping</p>
+              </Link>  
+            : 
+              <Link to='/signin'> 
+                <button className='text-sm md:text-[15px] bg-[#FFD814] hover:bg-yellow-500 border-[#c9a801] border h-8 rounded px-2'>
+                Sign in to continue shopping
+                </button>
+              </Link>
+            }  
+          </div>
+          :
+          // When cart has items
+          <div>
+            <h1 className='text-xl md:text-3xl font-semibold mb-7'>Shopping Cart</h1>
+            <div className='flex flex-col items-center md:items-start md:h-[220px] md:flex-row gap-5'>
+              {/* Cart Items*/}
+              <div className='flex flex-col gap-3 w-[75%]'>
+                {checkoutItems}
+              </div>
+              {/*Section to display total price of cart*/}
+              <div className='p-5 flex flex-col justify-center items-center gap-3 md:w-[23%] md:h-[220px] border bg-white rounded shadow-xl'>
+                <p className='text-[17px] md:text-[20px]'>Subtotal of ({cartCount} items): <strong>₹{totalPrice}</strong></p>
+                <button 
+                onClick={saveOrders}
+                className='bg-[#FFD814] hover:bg-yellow-500 border-[#c9a801] border h-9 rounded px-2 w-full'
+                    >Proceed to buy</button>
+              </div>
             </div>
           </div>
-        </div>
-      }
+        }
+      </div>
     </div>
   )
 }
